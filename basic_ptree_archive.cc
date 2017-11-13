d99ee5374f6126c9ce2ffa2d52cd523cc96621bb
Sergey Spiridonov <sena@s73.work>
2017-11-13 18:24:09 +0100

    ss: shorten reserved keywords

--- basic_ptree_archive.cc
+++ basic_ptree_archive.cc
@@ -20,14 +20,14 @@
 
 namespace bpta {
 
-  const char *ptree_archive_object_id          = "__oid" ;
-  const char *ptree_archive_object_reference   = "__oidr";
-  const char *ptree_archive_class_id           = "__cid" ;
-  const char *ptree_archive_class_id_reference = "__cidr";
-  const char *ptree_archive_class_name         = "__cn"  ;
-  const char *ptree_archive_tracking           = "__tl"  ;
-  const char *ptree_archive_version            = "__ver" ;
-  const char *ptree_archive_signature          = "__sgn" ;
+  const char *ptree_archive_object_id          = "__oi" ;
+  const char *ptree_archive_object_reference   = "__or" ;
+  const char *ptree_archive_class_id           = "__ci" ;
+  const char *ptree_archive_class_id_reference = "__cr" ;
+  const char *ptree_archive_class_name         = "__cn" ;
+  const char *ptree_archive_tracking           = "__t"  ;
+  const char *ptree_archive_version            = "__v"  ;
+  const char *ptree_archive_signature          = "__s"  ;
 
     // internal boost::serialization names
   const char *boost_internal_count             = "count";
