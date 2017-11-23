fa8eb5d71d152ee9ff36f0c8877a50888dbfa355
Sergey Spiridonov <sena@s73.work>
2017-11-23 18:10:59 +0100

    sena: relicensed with the same license as boost

--- test/test_derived_saveload.cc
+++ test/test_derived_saveload.cc
@@ -1,20 +1,10 @@
-/* Copyright (C) 2017 Sergey Spiridonov
+/* Copyright (C) 2017 Sergey Spiridonov <sena@s73.work>
  *
  * This file is part of bpta (ptree_archive) library.
  *
- * bpta is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * bpta is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with bpta.  If not, see <http://www.gnu.org/licenses/>.
- * */
+ * Distributed under the Boost Software License, Version 1.0. (See
+ * accompanying file COPYING
+ */
 
 #include <iostream>
 #include <boost/property_tree/json_parser.hpp>
